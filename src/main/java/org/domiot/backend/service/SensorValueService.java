0b1f95519d693855344434049b56fe12f9197022
jlankheet <jeroen.lankheet@triopsys.nl>
2025-01-19 11:35:09 +0100

    Database robustness for sensorvalue writing

--- src/main/java/org/domiot/backend/service/SensorValueService.java
+++ src/main/java/org/domiot/backend/service/SensorValueService.java
@@ -1,12 +1,20 @@
 package org.domiot.backend.service;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
+import jakarta.annotation.PreDestroy;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+
 import org.domiot.backend.database.SensorValueEntityRepository;
 import org.domiot.backend.mapper.SensorValueDtoMapper;
 import org.lankheet.domiot.domotics.dto.SensorValueDto;
 import org.lankheet.domiot.entities.SensorValueEntity;
-import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Retryable;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 /**
@@ -19,6 +27,7 @@ public class SensorValueService {
     private final SensorValueEntityRepository sensorValueEntityRepository;
 //    @Qualifier("sensorValueDtoMapperImpl")
     private final SensorValueDtoMapper sensorValueMapper;
+    private final Queue<SensorValueEntity> buffer = new LinkedList<>();
 
     /**
      * Save a new sensorValue
@@ -27,9 +36,33 @@ public class SensorValueService {
      * @param sensorValueDto The sensorValue to save
      * @return The saved sensorValue
      */
+    @Retryable(maxAttempts = 40, backoff = @Backoff(delay = 2000))
     public SensorValueDto saveSensorValue(SensorValueDto sensorValueDto) {
         SensorValueEntity sensorValueEntity = sensorValueMapper.map(sensorValueDto);
-        SensorValueEntity savedSensorValue = sensorValueEntityRepository.save(sensorValueEntity);
+        SensorValueEntity savedSensorValue = null;
+        try {
+            savedSensorValue = sensorValueEntityRepository.save(sensorValueEntity);
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+            buffer.add(sensorValueEntity);
+        }
         return sensorValueMapper.map(savedSensorValue);
+    }
+
+    @Scheduled(fixedRate = 5000) // Retry every 5 seconds
+    public void flushBuffer() {
+        while (!buffer.isEmpty()) {
+            try {
+                sensorValueEntityRepository.save(buffer.poll());
+            } catch (Exception e) {
+                // If it fails, break and try again in the next flush
+                break;
+            }
+        }
+    }
+
+    @PreDestroy
+    public void onShutdown() {
+        flushBuffer();
     }
 }
